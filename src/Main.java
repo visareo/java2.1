f01b2345c118368fc38942e84d9f4049ac081d43
visareo <130570639+visareo@users.noreply.github.com>
2023-05-10 02:11:19 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int amount = 25000;
-        int milesCost = 20;
-        int miles ;
+        int amount = 25000; //стоимость билета
+        int milesCost = 20; //стоимость одной мили
+        int miles ; //мили
 
-        miles = amount / milesCost;
+        miles = amount / milesCost; //сохранение в переменную miles кол-во начисленных миль
         System.out.println("Количество начисленных миль: " + miles);
 
 
